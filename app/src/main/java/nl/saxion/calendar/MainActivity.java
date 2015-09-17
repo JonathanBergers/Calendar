a26ec9fb0d32e41d50fca3ea11e3906a88294608
jonathan <jonathan.bergers@gmail.com>
2015-09-17 10:04:02 +0200

    android annotations working

--- app/src/main/java/nl/saxion/calendar/MainActivity.java
+++ app/src/main/java/nl/saxion/calendar/MainActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
